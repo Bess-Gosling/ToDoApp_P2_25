9799e75b04c0d057d0aae8992b0026c92bf6ff29
Bess Gosling <bess.gosling@outlook.com>
2025-03-06 19:57:17 +1000

    fixed up User class error

--- src/User.java
+++ src/User.java
@@ -2,11 +2,6 @@ public class User {
     private String username;
     private String password;
 
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
     /**
      * Gets the username of the user.
      */
